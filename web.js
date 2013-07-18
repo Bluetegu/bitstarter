1895c7107477488f7875be2ddfe98220f498620b
Ubuntu <ubuntu@ip-172-31-45-221.us-west-2.compute.internal>
2013-07-18 07:11:53 +0000

    changed index.html to fluid design (HW4) and listening on prot 8080

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ app.get('/', function(request, response) {
   response.send(buffer.toString());  // sending buffer directly doesn't present it as html 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
